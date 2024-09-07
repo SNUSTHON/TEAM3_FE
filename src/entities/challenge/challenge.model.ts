e86ad2a0f022d1fcac4f28f664ccc38bee3a2794
ShinYoung-Kim <lukey0515@naver.com>
2024-09-08 06:15:58 +0900

    feat: 카테고리 아이콘 매핑 추가

--- src/entities/challenge/challenge.model.ts
+++ src/entities/challenge/challenge.model.ts
@@ -3,7 +3,15 @@ export type TCategoryName = string;
 
 export type MemberChallengesId = number;
 export type ChallengeName = string;
-export type CategoryName = "집안일" | "운동" | "학습";
+export type CategoryName =
+	| "집안일"
+	| "운동"
+	| "학습"
+	| "정신건강"
+	| "감사"
+	| "생활습관"
+	| "사소한 목표"
+	| "디지털 디톡스";
 export type Duration = number;
 export type IsDone = boolean;
 
--- src/widgets/history/card/card.constant.ts
+++ src/widgets/history/card/card.constant.ts
@@ -1,6 +1,11 @@
 import homeIcon from "../../../assets/home.svg";
 import exerciseIcon from "../../../assets/exercise.svg";
 import pencilIcon from "../../../assets/pencil.svg";
+import digitalIcon from "../../../assets/digital.svg";
+import heartIcon from "../../../assets/heart.svg";
+import mentalHealthIcon from "../../../assets/mental-health.svg";
+import routineIcon from "../../../assets/routine.svg";
+import targetIcon from "../../../assets/target.svg";
 import { TCategoryName } from "../../../entities/challenge/challenge.model";
 
 export type CategoryIcon = typeof homeIcon | typeof exerciseIcon | typeof pencilIcon;
@@ -10,4 +15,9 @@ export const categoryIconMapping: TCategoryIconMapping = {
 	집안일: homeIcon,
 	운동: exerciseIcon,
 	학습: pencilIcon,
+	"정신 건강": mentalHealthIcon,
+	감사: heartIcon,
+	"사소한 목표": targetIcon,
+	"디지털 디톡스": digitalIcon,
+	"생활 습관": routineIcon,
 };
