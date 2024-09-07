f70d784ca8a94be5874e45205bacc3499aede21f
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 17:39:03 +0900

    design: 격언 디자인 여백 수정

--- src/pages/today/Phrase.tsx
+++ src/pages/today/Phrase.tsx
@@ -2,7 +2,7 @@ import { PhraseProps } from "../../entities/phrase/phrase.model";
 
 const Phrase = ({ author, message }: PhraseProps) => {
 	return (
-		<div className="px-8 pt-6 pb-3 flex flex-col gap-4">
+		<div className="pt-6 pb-3 flex flex-col gap-4">
 			<h1 className="font-extrabold text-2xl text-default-900 break-keep">{message}</h1>
 			<h6 className="font-normal text-sm text-default-400">{author}</h6>
 		</div>
