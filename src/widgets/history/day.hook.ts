179d8c804032f07f4c21618ef84bc8192b043b72
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 22:29:02 +0900

    style: counts 데이터 가데이터 파일 분리

--- src/widgets/history/day.hook.ts
+++ src/widgets/history/day.hook.ts
@@ -1,29 +1,9 @@
-import { CreatedAt, DayHistoryList } from "../../entities/day/day.model";
+import { CreatedAt } from "../../entities/day/day.model";
+import { mockCounts } from "./day.mock";
 import { Count } from "./day.type";
 
 export const useGetCount = (createdAt: CreatedAt): Count => {
-	const counts: DayHistoryList = [
-		{
-			createdAt: "2024-09-01",
-			count: 5,
-		},
-		{
-			createdAt: "2024-09-02",
-			count: 6,
-		},
-		{
-			createdAt: "2024-08-11",
-			count: 2,
-		},
-		{
-			createdAt: "2024-09-07",
-			count: 3,
-		},
-		{
-			createdAt: "2024-08-16",
-			count: 1,
-		},
-	];
+	const counts = mockCounts;
 	const count = Math.min(counts.find((day) => day.createdAt === createdAt)?.count ?? 0, 5);
 	return count as Count;
 };
---
+++ src/widgets/history/day.mock.ts
@@ -0,0 +1,28 @@
+import { DayHistoryList } from "../../entities/day/day.model";
+
+export const mockCounts: DayHistoryList = [
+	{
+		createdAt: "2024-09-01",
+		count: 5,
+	},
+	{
+		createdAt: "2024-09-02",
+		count: 6,
+	},
+	{
+		createdAt: "2024-08-11",
+		count: 2,
+	},
+	{
+		createdAt: "2024-09-07",
+		count: 3,
+	},
+	{
+		createdAt: "2024-08-16",
+		count: 1,
+	},
+	{
+		createdAt: "2024-08-20",
+		count: 4,
+	},
+];
