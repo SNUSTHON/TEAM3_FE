244870a8bfa231b62cde0059081b45b74fc59472
ShinYoung-Kim <lukey0515@naver.com>
2024-09-08 00:52:28 +0900

    feat: 페이지에 challenge 컴포넌트 적용

--- src/pages/my/page.tsx
+++ src/pages/my/page.tsx
@@ -1,12 +1,16 @@
 import Header from "./Header";
 import { mockUser } from "../../entities/user/user.mock";
 import User from "./User";
+import Sponser from "./Sponser";
+import Challenge from "./Challenge";
 
 const MyPage = () => {
 	return (
 		<div>
 			<Header />
 			<User {...mockUser} />
+			{Sponser(mockUser.sponsorProgress)}
+			<Challenge />
 		</div>
 	);
 };
