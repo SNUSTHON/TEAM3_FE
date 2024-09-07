f3e842092dd79811ef1eccfa262f1a502922c3c2
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 21:01:05 +0900

    design: challenge 디자인 변경

--- src/widgets/challenge/Challenge.tsx
+++ src/widgets/challenge/Challenge.tsx
@@ -9,15 +9,16 @@ const Challenge = ({ challengeName, level, duration, isDone }: ChallengeProps) =
 		<Checkbox
 			isSelected={isSelected}
 			onValueChange={setIsSelected}
-			className="w-full max-w-full"
+			className="w-full max-w-full bg-primary-50 p-4 rounded-2xl"
 			classNames={{
 				label: "w-full",
+				icon: "before:border-white",
 			}}
 		>
 			<div className="flex w-full">
 				<div className="flex flex-col w-full gap-1">
 					<h3 className="text-default-900 text-sm font-bold">{challengeName}</h3>
-					<h6 className="text-default-600 text-xs font-medium">난이도 {level}</h6>
+					<h6 className="text-xs font-medium text-primary-500">난이도 {level}</h6>
 				</div>
 				<h6 className="text-default-900 font-medium text-xs flex items-baseline gap-1">
 					<span className="font-extrabold text-2xl">{duration}</span>min
