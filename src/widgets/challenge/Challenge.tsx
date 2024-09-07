fe49eb65c5daf4dd5974408141d01ea25a5916a8
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 22:43:37 +0900

    style: 난이도 표현 LV로 변경

--- src/widgets/challenge/Challenge.tsx
+++ src/widgets/challenge/Challenge.tsx
@@ -18,7 +18,7 @@ const Challenge = ({ challengeName, level, duration, isDone }: ChallengeProps) =
 			<div className="flex w-full">
 				<div className="flex flex-col w-full gap-1">
 					<h3 className="text-default-900 text-sm font-bold">{challengeName}</h3>
-					<h6 className="text-xs font-medium text-primary-500">난이도 {level}</h6>
+					<h6 className="text-xs font-medium text-primary-500">Lv {level}</h6>
 				</div>
 				<h6 className="text-default-900 font-medium text-xs flex items-baseline gap-1">
 					<span className="font-extrabold text-2xl">{duration}</span>min
