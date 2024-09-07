8c5637669942231ee947ecdc7e7c08502fd3b5e4
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 18:01:24 +0900

    chore: phrase 가데이터 변경

--- src/entities/phrase/phrase.mock.ts
+++ src/entities/phrase/phrase.mock.ts
@@ -1,6 +1,6 @@
 import { PhraseProps } from "./phrase.model";
 
 export const mockPhrase: PhraseProps = {
-	author: "에이브러햄 링컨",
-	message: "반드시 이겨야 하는 건 아니지만 진실할 필요는 있다. 반드시 성공해야 하는 건 아니지만, 소신을 가지고 살아야 할 필요는 있다.",
+	author: "Franklin D. Roosevelt",
+	message: "내일의 실현을 가로막는 유일한 한계는 오늘을 의심하는 것입니다",
 };
