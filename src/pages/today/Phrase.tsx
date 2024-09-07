a5d24f2d9cb8abac2c4c46f70cf202de02f0df3f
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 16:39:13 +0900

    feat: phrase 컴포넌트 생성

--- src/pages/today/Phrase.tsx
+++ src/pages/today/Phrase.tsx
@@ -1,5 +1,12 @@
-const Phrase = () => {
-	return <div></div>;
+import { PhraseProps } from "../../entities/phrase/phrase.model";
+
+const Phrase = ({ author, message }: PhraseProps) => {
+	return (
+		<div className="px-8 pt-6 pb-3 flex flex-col gap-4">
+			<h1 className="font-extrabold text-2xl text-default-900 break-keep">{message}</h1>
+			<h6 className="font-normal text-sm text-default-400">{author}</h6>
+		</div>
+	);
 };
 
 export default Phrase;
