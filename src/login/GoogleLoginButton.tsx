a474a6ebd33784687c63b8f5a8396cf2452c36a7
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 15:00:39 +0900

    design: 구글 로그인 디자인 추가

--- src/login/GoogleLoginButton.tsx
+++ src/login/GoogleLoginButton.tsx
@@ -3,8 +3,11 @@ import googleLogo from "../assets/google.svg";
 
 const GoogleLoginButton = () => {
 	return (
-		<Button>
-			<img src={googleLogo} alt="Google Logo" />
+		<Button
+			startContent={
+				<img src={googleLogo} alt="Google Login" style={{ width: "20px", height: "20px" }} />
+			}
+		>
 			구글로 시작하기
 		</Button>
 	);
