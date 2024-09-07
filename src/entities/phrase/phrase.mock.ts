5c6de16c66f3ff9603f2a998783ec46cfa1007b6
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 16:38:57 +0900

    feat: 격언 model 및 가데이터 생성

---
+++ src/entities/phrase/phrase.mock.ts
@@ -0,0 +1,6 @@
+import { PhraseProps } from "./phrase.model";
+
+export const mockPhrase: PhraseProps = {
+	author: "에이브러햄 링컨",
+	message: "반드시 이겨야 하는 건 아니지만 진실할 필요는 있다. 반드시 성공해야 하는 건 아니지만, 소신을 가지고 살아야 할 필요는 있다.",
+};
---
+++ src/entities/phrase/phrase.model.ts
@@ -0,0 +1,7 @@
+export type Author = string;
+export type Message = string;
+
+export type PhraseProps = {
+	author: Author;
+	message: Message;
+};
