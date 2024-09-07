dbc3389b95b18072ca74bc9b1259e2e5422a860d
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 16:38:39 +0900

    chore: tailwind 설정 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { NextUIProvider } from "@nextui-org/react";
+import "./app/output.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@ import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", "./src/**/*.{html,js}"],
+	content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				Pretendard: "Pretendard",
+			},
+		},
 	},
 	darkMode: "class",
 	plugins: [nextui()],
