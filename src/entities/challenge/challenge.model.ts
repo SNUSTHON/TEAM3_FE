dff1730140dc054cf843c4bf44e1916e4697a4bc
ShinYoung-Kim <lukey0515@naver.com>
2024-09-08 02:11:58 +0900

    type: category name 타입 수정

--- src/entities/challenge/challenge.model.ts
+++ src/entities/challenge/challenge.model.ts
@@ -3,7 +3,7 @@ export type TCategoryName = string;
 
 export type MemberChallengesId = number;
 export type ChallengeName = string;
-export type CategoryName = string;
+export type CategoryName = "집안일" | "운동" | "학습";
 export type Duration = number;
 export type IsDone = boolean;
 
