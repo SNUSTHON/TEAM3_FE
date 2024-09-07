477649b324b9974dc93fc4d927a2d4a8d017bad8
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 18:01:38 +0900

    design: phrase 간격 변경

--- src/pages/today/Phrase.tsx
+++ src/pages/today/Phrase.tsx
@@ -2,7 +2,7 @@ import { PhraseProps } from "../../entities/phrase/phrase.model";
 
 const Phrase = ({ author, message }: PhraseProps) => {
 	return (
-		<div className="pt-6 pb-3 flex flex-col gap-4">
+		<div className="pt-6 pb-3 flex flex-col gap-1">
 			<h1 className="font-extrabold text-2xl text-default-900 break-keep">{message}</h1>
 			<h6 className="font-normal text-sm text-default-400">{author}</h6>
 		</div>
