3786aa1fff9300f3edeb9aac49fba04e17baa9de
ShinYoung-Kim <lukey0515@naver.com>
2024-09-07 23:48:29 +0900

    feat: 유저 정보 컴포넌트 개발

---
+++ src/pages/my/User.tsx
@@ -0,0 +1,15 @@
+import { TUser } from "../../entities/user/user.model";
+import userImage from "../../assets/avatar.svg";
+
+type UserProps = Pick<TUser, "username">;
+
+const User = ({ username }: UserProps) => {
+	return (
+		<div className="flex flex-col items-center gap-4">
+			<img src={userImage} alt="avatar" width={125} height={125} />
+			<h3>{username}</h3>
+		</div>
+	);
+};
+
+export default User;
