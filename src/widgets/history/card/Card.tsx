af00a96e0ee02fb884789a8abb56238fc44b172d
ShinYoung-Kim <lukey0515@naver.com>
2024-09-08 02:28:32 +0900

    feat: divider 추가

--- src/widgets/history/card/Card.tsx
+++ src/widgets/history/card/Card.tsx
@@ -7,6 +7,7 @@ const Card = ({ challengeName, level, categoryName, duration, isDone }: TCard) =
 		<div className="flex flex-col gap-6 border rounded-2xl border-default p-4">
 			<h6 className="font-bold text-xs">{getLevelString(level)}</h6>
 			<h3 className="font-bold text-2xl">{challengeName}</h3>
+			<div className="w-full h-[2px] bg-default" />
 			<CardInfo categoryName={categoryName} duration={duration} isDone={isDone} />
 		</div>
 	);
---
+++ src/widgets/history/card/Divider.tsx
@@ -0,0 +1,5 @@
+const Divider = () => {
+	return <div className="w-[2px] h-10 bg-default" />;
+};
+
+export default Divider;
